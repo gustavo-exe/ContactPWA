9a90e1f14ca6b8410faf89889abd97913ebe4808
gustavo-exe <gus.elim2@gmail.com>
2021-05-22 21:32:44 -0600

    Boton flotante

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
     var modal = document.querySelectorAll('.modal');
+    var fixedActionBtn = document.querySelectorAll('.fixed-action-btn');
+    M.FloatingActionButton.init(fixedActionBtn);
     M.Sidenav.init(sidenav);
     M.Modal.init(modal);
 });
